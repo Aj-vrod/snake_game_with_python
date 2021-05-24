a5789d66cb4a6eb2ef5d01fbb9d64f47b2972567
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-24 22:46:43 +0200

    added conditions to event key

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pygame
 from pygame.locals import *
 
+def draw_block():
+    surface.fill((110,110,5))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
 if __name__ == "__main__":
     pygame.init()
 
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((255, 255, 255))
+    surface = pygame.display.set_mode((1000, 500))
+    surface.fill((110,110,5))
     block = pygame.image.load("resources/block.jpg").convert()
-    surface.blit(block, (100,100))
+    block_x = 100
+    block_y = 100
+    surface.blit(block, (block_x, block_y))
     pygame.display.flip()
 
     running = True
@@ -16,5 +23,7 @@ if __name__ == "__main__":
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+                if event.key == K_UP:
+                    pass
             elif event.type == QUIT:
                 running = False
