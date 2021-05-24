6b9ce6b8bb840c8bd22f2afe8d89f91bf429870e
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-24 20:30:46 +0200

    inserted first condition to keep it running

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ if __name__ == "__main__":
     pygame.init()
 
     surface = pygame.display.set_mode((500, 500))
+    surface.fill((255, 255, 255))
+    pygame.display.flip()
+
+    
     time.sleep(5)
