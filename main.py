e67721f33b5106903c6c22a677c1d754e22ce2e3
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-24 23:21:03 +0200

    created function to keep snake walking

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Snake:
         self.block = pygame.image.load("resources/block.jpg").convert()
         self.x = 100
         self.y = 100
+        self.direction = 'down'
     
     def draw(self):
         self.parent_screen.fill((110,110,5))
@@ -15,7 +16,15 @@ class Snake:
         pygame.display.flip()
     
     def walk(self):
-        
+        if self.direction == 'down':
+            self.y += 10
+        if self.direction == 'up':
+            self.y -= 10
+        if self.direction == 'left':
+            self.x -= 10
+        if self.direction == 'right':
+            self.x += 10
+        self.draw()
     
     def move_left(self):
         self.x -= 10
@@ -56,6 +65,7 @@ class Game:
                 elif event.type == QUIT:
                     running = False
             self.snake.walk()
+            time.sleep(0.2)
 
 
 if __name__ == "__main__":
