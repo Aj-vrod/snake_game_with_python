b98873da4a7af2b553846384724af94d38c000cb
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-24 22:53:18 +0200

    moving code into classes

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from pygame.locals import *
 
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface.fill((110,110,5))
+    
+    def run(self):
+        
+
 def draw_block():
     surface.fill((110,110,5))
     surface.blit(block, (block_x, block_y))
     pygame.display.flip()
 
 if __name__ == "__main__":
-    pygame.init()
+    
 
-    surface = pygame.display.set_mode((1000, 500))
-    surface.fill((110,110,5))
+    
     block = pygame.image.load("resources/block.jpg").convert()
     block_x = 100
     block_y = 100
