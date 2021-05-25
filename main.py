99bfbdf15f801429cd8a4cd3d27c2982cf545e26
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-05-25 15:38:32 +0200

    created drwaw function for apple

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ class Apple:
     def __init__(self, parent_screen):
         self.image = pygame.image.load("resources/apple.jpg").convert()
         self.parent_screen = parent_screen
-        self.x = 120
-        self.y = 120
+        self.x = SIZE * 3
+        self.y = SIZE * 3
+    
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
 
 class Snake:
     def __init__(self, parent_screen, length):
